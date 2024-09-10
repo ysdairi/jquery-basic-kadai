7bd5e099696cde11619a9729c8c7911f2a591a89
ysdairi <airiyoshida929@gmail.com>
2024-09-10 14:32:52 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+// $(function(){
+
+　// HTMLドキュメントが読み込み完了したとき
+  $(window).on('load',function(){
+    console.log('loadイベントが発生しました')
+  });
+  // 画面をスクロールしたとき
+  $(window).on('scroll',function(){
+    console.log('scrollイベントが発生しました')
+  });
+
+// })
